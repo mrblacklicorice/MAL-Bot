e7f830f6bf79259513a3d6bd20f74394ed48ec65
mrblacklicorice <61810026+mrblacklicorice@users.noreply.github.com>
2020-09-30 15:03:50 -0500

    small changes

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ const prefix = "m! ";
 
 client.on('ready', () => {
     console.log(`Logged in as ${client.user.tag}!`);
-    client.user.setActivity("In construction | m! help");
+    client.user.setActivity("m! help");
 });
 
 
